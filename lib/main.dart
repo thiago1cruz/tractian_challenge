d5d20181fccb089231a6d8ff6dab4300d618c789
Thiago Araujo <thiago.tg.k2@hotmail.com>
2024-09-16 17:18:00 -0300

    refactor: change app route system

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:tractian_challenge/app/app_widget.dart';
+import 'package:tractian_challenge/app/modules/app_module.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent,
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(    
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:Container(),
-    );
-  }
-}
