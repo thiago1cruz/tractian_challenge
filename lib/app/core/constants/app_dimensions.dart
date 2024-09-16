48db1ead84eae20504ab251f0fe0de44ce6f8321
Thiago Araujo <thiago.tg.k2@hotmail.com>
2024-09-16 16:35:57 -0300

    feat: app size settings

---
+++ lib/app/core/constants/app_dimensions.dart
@@ -0,0 +1,8 @@
+class AppDimensions  {
+  
+    static const double defaultPadding = 16.0;
+    static const double smallPadding = 8.0;
+    static const double largePadding = 24.0;
+
+
+}
---
+++ lib/app/core/constants/app_fonts.dart
@@ -0,0 +1,7 @@
+class AppFontsSize {
+  static const double extraSmall = 10.0;
+  static const double small = 12.0;
+  static const double medium = 14.0;
+  static const double large = 16.0;
+  static const double extraLarge = 18.0;
+}
