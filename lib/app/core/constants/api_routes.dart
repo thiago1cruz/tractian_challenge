0802131b1958482a655cad90048396a231ec8942
Thiago Araujo <thiago.tg.k2@hotmail.com>
2024-09-16 16:35:02 -0300

    feat: api routes

---
+++ lib/app/core/constants/api_routes.dart
@@ -0,0 +1,9 @@
+const baseUrl = 'fake-api.tractian.com';
+
+const companies = '/companies';
+const locations = '/locations';
+const assets = '/assets';
+
+
+const configTimeout = 5000;
+const configReceiveTimeout = 3000;
