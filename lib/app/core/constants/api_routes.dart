44db77cabbb3aa8b7b58bcc9e95ee979be3214da
Thiago Araujo <thiago.tg.k2@hotmail.com>
2024-09-17 12:00:56 -0300

    refactor: change baseurl

--- lib/app/core/constants/api_routes.dart
+++ lib/app/core/constants/api_routes.dart
@@ -1,9 +1,9 @@
-const baseUrl = 'fake-api.tractian.com';
+const baseUrl = 'https://fake-api.tractian.com';
 
 const companies = '/companies';
 const locations = '/locations';
 const assets = '/assets';
 
 
-const configTimeout = 5000;
-const configReceiveTimeout = 3000;
+const configTimeout = 10;
+const configReceiveTimeout = 20;
