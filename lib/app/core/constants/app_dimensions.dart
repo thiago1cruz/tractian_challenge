338a7960eaa5ff2ae0a99550d1281ddd9b5947ce
Thiago Araujo <thiago.tg.k2@hotmail.com>
2024-09-17 12:02:22 -0300

    refactor: add more settings in appDimensions

--- lib/app/core/constants/app_dimensions.dart
+++ lib/app/core/constants/app_dimensions.dart
@@ -2,7 +2,17 @@ class AppDimensions  {
   
     static const double defaultPadding = 16.0;
     static const double smallPadding = 8.0;
-    static const double largePadding = 24.0;
+    static const double largePadding = 25.0;
+
+
+    static const double defaultMagin = 12.0;
+    static const double smallMargin = 6.0;
+    static const double largeMargin = 20.0;
+
+
+    static const double defaultBorderRadius = 10.0;
+    static const double smallBorderRadius = 5.0;
+    static const double largeBorderRadius = 20.0;
 
 
 }
